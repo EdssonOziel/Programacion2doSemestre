0c5aeff7f93482999b162b62948aa3f0118ac1ed
EdssonOziel <edssonoziel@gmail.com>
2020-03-08 00:30:49 +0000

    UsoFormat

---
+++ UsoFormat.py
@@ -0,0 +1,8 @@
+#Uso de la funcion format
+def main():
+  intBase = 7
+  intAltura = 5
+  fltAreaTriangulo=(intBase*intAltura)/2
+  txt = "Area: {2:0.2f} ( {0} por {1} entre dos )"
+#Valores incrustados en la salida
+  print(txt.format(intBase, intAltura, fltAreaTriangulo))
