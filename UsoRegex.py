dc218f1daa3390fb05ac4fdf916f1d1ea134f69b
EdssonOziel <edssonoziel@gmail.com>
2020-03-08 01:16:15 +0000

    UsoRegex

---
+++ UsoRegex.py
@@ -0,0 +1,15 @@
+# Pregunta un dato hasta que sea correcto, haciendo validaciones usando expresiones regulares.
+# Importa el módulo requerido para usar Regular Expressions.
+import re
+
+def main():
+    #Infinity Loop - itera hasta que se presenta un break
+    #Estara preguntando el dato, mientras no cumpla con el patrson de la expresion regular.
+    while True:
+        strRFC = input("Dame el RFC: ")
+        coincide = re.search("^[A-Z]{4}-[0-9]{6}-[A-Z0-9]{3}$", strRFC)
+        if (coincide):
+            prin("RFC Correcto!")
+            break
+        else:
+            print("RFC incorrecto. Intenta de nuevo.")
